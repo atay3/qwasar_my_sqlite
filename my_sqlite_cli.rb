870062bdbc49ca631777f8f11826b6fe1ee2f108
tay_a <ashleytay448@gmail.com>
2025-05-21 22:37:27 -0700

    Set up MySqliteCli

--- my_sqlite_cli.rb
+++ my_sqlite_cli.rb
@@ -1,6 +1,32 @@
-require 'my_sqlite_request'
+require_relative 'my_sqlite_request'
 require 'readline'
 
-while buf = Readline.readline(">", true)
-    p buf
+class MySqliteCli
+    def run_prompt()
+        puts "MySQLite version 0.1 20XX-XX-XX"
+
+        while input = Readline.readline("my_sqlite_cli> ", true)
+            process_input(input)
+        end
+    end
+
+    def process_input(input)
+        case input.strip.downcase
+        when 'exit', 'quit'
+            break
+        when ''
+            next
+        else
+            execute_query(input)
+        end
+    end
+
+    def execute_query(query)
+        request = MySqliteRequest.new
+        # Parse and execute the query
+        
+        # Handle wildcard
+
+        #Handle reqests for specific columns
+    end
 end
