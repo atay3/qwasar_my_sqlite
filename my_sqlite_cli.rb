c81266632912f41db9a1bd4a640907d569a2b41a
tay_a <ashleytay448@gmail.com>
2025-05-21 23:26:08 -0700

    Added function to run prompts and set up some functions to make code more modular

--- my_sqlite_cli.rb
+++ my_sqlite_cli.rb
@@ -13,9 +13,9 @@ class MySqliteCli
     def process_input(input)
         case input.strip.downcase
         when 'exit', 'quit'
-            break
+            exit 0
         when ''
-            next
+            return
         else
             execute_query(input)
         end
@@ -24,9 +24,31 @@ class MySqliteCli
     def execute_query(query)
         request = MySqliteRequest.new
         # Parse and execute the query
-        
+        case query
+        when /^SELECT/ then handle_select(query)
+        # when /^UPDATE/ then handle_update(query)
+        # when /^INSERT/i then handle_insert(query)
+        # when /^DELETE/i then handle_delete(query)
+        end
         # Handle wildcard
+        # Handle reqests for specific columns
+    end
 
-        #Handle reqests for specific columns
+    def handle_select(query)
+        # if query.match(/SELECT \* FROM (\w+)/i)
+        #     table = $1
+        #     request.from("#{table}.csv").select('*')
+        #     results = request.run
+        #     display_results(results)
+        # end
+    end
+
+    if __FILE__ == $PROGRAM_NAME
+        # Get filename from ARGV
+        table_name = ARGV[0]&.gsub(/\.csv$/, '')
+        
+        # Run CLI
+        cli = MySqliteCli.new()
+        cli.run_prompt
     end
 end
