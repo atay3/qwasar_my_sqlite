a26cf81a122db9b5ddb47aac2c0cc9c4d580cab0
icode2code@gmail.com <icode2code@gmail.com>
2025-05-25 23:01:17 -0700

    renamed test_file_local to test_local for easier typing etc

---
+++ test_local.rb
@@ -0,0 +1,52 @@
+require_relative "my_sqlite_request"
+
+request = MySqliteRequest.new
+#  ✅test run() - check for empty
+# p request.run
+
+#   emojis for testing status
+#⚠️
+#❌
+#✅
+
+#   test from()
+request = request.from("student.csv")
+# request = request.select("id")
+table_all = request.get_table_data
+headers = table_all[:headers]
+table_data = table_all[:data][1..-1]
+
+def p_headers(headers)
+    puts "headers\n#{headers}\n"
+end
+
+def p_table_data(table_data)
+    puts "table"
+    table_data.each {|x| puts x.inspect}
+end
+
+def p_errors(errors)
+    puts "errors - #{errors}"
+end
+
+def p_queue(queue)
+    puts "queue - #{queue}"
+end
+
+def p_q_result(result)
+    puts "result - #{result}"
+end
+
+p_headers(headers)
+p_table_data(table_data)
+p_errors(request.get_request_errors)
+p_queue(request.get_request_queue)
+request = request.select('ages')
+# p_q_result(result.queue_result)
+# request.run
+
+#CHECKED
+#   -from()
+
+#TODO
+#   create run_from()
